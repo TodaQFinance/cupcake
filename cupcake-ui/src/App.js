62b3488f97e9dddafafec69fa4e8d0f87d61b87c
Chris Kellendonk <chris.kellendonk@todaqfinance.com>
2019-01-31 23:15:29 -0500

    Create cupcake component

--- cupcake-ui/src/App.js
+++ cupcake-ui/src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactSVG from 'react-svg'
 import './App.css';
+
+import Cupcake from './Cupcake'
+
+/*
+
+var flavourColours = {"chocolate": "#FF0000", "vanilla": "#FFFFBB"};
+var icingColours = {"chocolate": "#FF0000", "vanilla": "#FFFFBB"};
+var sprinklesColours = {"chocolate": "#FF0000"};
+var candleColours = {"blue":"#0000FF"};
+
+
+var renderCupcake = function(cupcake) {
+    var el = $("#cupcake").clone();
+    if (cupcake.flavour && cupcake.flavour in flavourColours) {
+	setFilling(el, flavourColours[cupcake.flavour]);
+    }
+    if (cupcake.icing && cupcake.icing in icingColours) {
+	setIcing(el, icingColours[cupcake.icing]);
+    }
+    if (cupcake.sprinkles && cupcake.sprinkles.quantity) {
+	numSprinkles(el, cupcake.sprinkles.quantity);
+    }
+    if (cupcake.sprinkles && cupcake.sprinkles.type in sprinklesColours) {
+	setSprinkles(el, sprinklesColours[cupcake.sprinkles.type]);
+    }
+    if (cupcake.candle && cupcake.candle.colour in candleColours) {
+	setCandle(el, candleColours[cupcake.candle.colour]);
+    }
+    el.css({'width':300}); // for now
+    el.show();
+    return el;
+};
+*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Cupcake icingColor="red" fillingColor="blue" paperColor="red" candleColor="red" numSprinkles={40} sprinklesColor="blue" />
+        <form>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputCity">Flavour</label>
+              <input type="text" className="form-control" id="inputCity" />
+            </div>
+            <div className="form-group col-md-4">
+              <label htmlFor="inputState">State</label>
+              <select id="inputState" className="form-control">
+                <option value="Chocolate">Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="form-group col-md-2">
+              <label htmlFor="inputZip">Zip</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
---
+++ cupcake-ui/src/Cupcake.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ReactSVG from 'react-svg'
+import './Cupcake.css';
+import cupcakeSvg from './cupcake.svg'
+
+function setCss(element, selector, style, value) {
+  element.querySelectorAll(selector).forEach(e => {
+    e.style[style] = value;
+  });
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onInjected = ({icingColor, fillingColor, paperColor, candleColor, numSprinkles, sprinklesColor}, error, element) => {
+    // Icing
+    setCss(element, "#Icing linearGradient stop[offset='0']", 'stopColor', icingColor);
+
+    // Filling
+    setCss(element, "#Base > linearGradient stop[offset='0']", "stopColor", fillingColor);
+    setCss(element, "#Base > linearGradient stop[offset='1']", "stopColor", fillingColor);
+    
+    // Paper
+    setCss(element, "#Base g linearGradient stop[offset='0']", "stopColor", paperColor);
+    setCss(element, "#Base g linearGradient stop[offset='1']", "stopColor", paperColor);
+
+    // Candle
+    setCss(element, "#Candle > linearGradient stop[offset='0']", "stopColor", candleColor);
+    //also have stops at 1, 0.1056 (light), 0.367, 0.8522
+    setCss(element, "#Candle > linearGradient stop[offset='0.367']", "stopColor", candleColor);
+    setCss(element, "#Candle > linearGradient stop[offset='0.8522']", "stopColor", candleColor);
+
+    // Sprinkles
+    setCss(element, ".st50, .st51, .st52, .s53, .st54", "fill", sprinklesColor);
+    setCss(element, "#Sprinkles *", "display", "none");
+    if(numSprinkles > 0) {
+      setCss(element, "#Sprinkles :nth-child(1)", "display", "initial");
+      for(var i = 0; i < numSprinkles; i++) {
+        setCss(element, "#Sprinkles :nth-child(" + (i + 4) + ")", "display", "initial");
+      }
+    }
+
+    
+  }
+
+  render() {
+    return (
+        <ReactSVG src={cupcakeSvg} svgClassName="cupcake-svg" onInjected={this.onInjected.bind(null, this.props)} />
+    );
+  }
+}
+
+export default App;
