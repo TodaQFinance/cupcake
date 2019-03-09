98c299fde7341ace79211f6300d6cab9db04f50d
Alex W <alex.williams@todaqfinance.com>
2019-03-09 12:31:39 +0000

    Add example call to list account files

--- cupcake-ui/src/lib/new.js
+++ cupcake-ui/src/lib/new.js
@@ -1,6 +1,6 @@
 const { sampleAccount } = require('./data');
 const createIndividualAccount = require('./helpers/accounts/createIndividualAccount');
-// const getAccounts = require('./helpers/accounts/getAccounts');
+// const getFilesByAccount = require('./helpers/accounts/getFilesByAccount');
 // const getAccountById = require('./helpers/accounts/getAccountById');
 
 require('dotenv').config();
@@ -9,6 +9,6 @@ require('dotenv').config();
 // Code can be run with ```npm run new```
 // All TaaS endpoints can be called from a function in src/lib/helpers
 
-// getAccounts().then(data => console.log(data));
-// getAccountById(id).then(data => console.log(data));
 createIndividualAccount(sampleAccount).then(data => console.log(data));
+// getFilesByAccount(process.env.ACCOUNT_0).then(data => console.log(data));
+// getAccountById(id).then(data => console.log(data));
