1455f11c6fa0e9e16e920e75c4868869c4ca5457
sfertman <sfertman@zoho.com>
2020-01-17 19:13:09 -0500

    rm commented code

--- cupcake-ui/src/components/AccountPanel.js
+++ cupcake-ui/src/components/AccountPanel.js
@@ -34,26 +34,4 @@ class AccountPanel extends Component {
   }
 }
 
-// const AccountPanel = ({
-//   account, cupcakes, onSelect, onLoad, selected,
-// }) => (
-//   <div>
-//     <AccountSelector account={account} onLoad={onLoad} />
-//     <div>
-//       {cupcakes.map((c) => {
-//         console.log(c);
-//         const className = classnames('cupcake-row', {
-//           "cupcake-row-selected": c.id == (selected && selected.id),
-//         });
-
-//         return (
-//           <div className={className} key={c.id} onClick={evt => onSelect(c)}>
-//             {c.id}
-//           </div>
-//         );
-//       })}
-//     </div>
-//   </div>
-// );
-
 export default AccountPanel;
